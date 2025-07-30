e9500b9f55528dc55399c9534d65cefefde04f41
SebbyLam <sebastian.lam1@education.nsw.gov.au>
2025-07-30 12:49:40 +1000

    Falling boject animation,continue coding

---
+++ Assets/Scripts/ObjectFallingController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFallingController : MonoBehaviour
+{
+    float wait = 0.1f;
+    public GameObject fallingObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void fall()
+    {
+        Instantiate(fallingObject) 
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
